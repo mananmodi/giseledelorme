70cc58b06e808ae7d6ab07898c950f417352dab8
Sukhdev <sukhdevsingh85298@gmail.com>
2026-01-20 12:11:39 +0530

    DB and files committed

---
+++ wp-content/languages/wpml/page-builder-shortcode-strings-32445-ka_GE.l10n.php
@@ -0,0 +1,16 @@
+<?php
+return ['messages'=>['#'=>'#','Return form'=>'დაბრუნების ფორმა','You have 14 working days, starting on the day of receipt of your delivery, to make your product return.
+
+<strong>Instructions for the return of non-compliant/damaged product or for the return following the exercise of your rights of withdrawal (for complete information, see GTC Article 6):</strong>
+<ul>
+ 	<li>To return the non-compliant or damaged product(s), use the available return label by clicking the button below.</li>
+ 	<li>To complete the return of your order following the confirmation of your rights of withdrawal, return the entire order, including samples and / or promotional objects by clicking on the button below.</li>
+</ul>
+'=>'თქვენ გაქვთ 14 სამუშაო დღე მიღების დღიდან თქვენი პროდუქტის დასაბრუნებლად.<br><br>
+
+<strong>შეუსაბამო/დაზიანებული პროდუქტის დაბრუნების ან თქვენი გაუქმების უფლების გამოყენების შემდეგ დაბრუნების ინსტრუქცია (სრული ინფორმაციისთვის იხილეთ ზოგადი ხელშეკრულების მე-6 მუხლი):</strong>
+<ul>
+ 	<li>შეუსაბამო ან დაზიანებული პროდუქტის(ების) დასაბრუნებლად, გამოიყენეთ ხელმისაწვდომი დაბრუნების ეტიკეტი ქვემოთ მოცემულ ღილაკზე დაჭერით.</li>
+ 	<li>თქვენი გაუქმების უფლების დადასტურების შემდეგ თქვენი შეკვეთის დაბრუნების დასასრულებლად, დააბრუნეთ მთელი შეკვეთა, მათ შორის ნიმუშები და/ან სარეკლამო ნივთები ქვემოთ მოცემულ ღილაკზე დაჭერით.</li>
+</ul>
+']];
